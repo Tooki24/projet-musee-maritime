db47ee5f0fe7d787a916b8c9d6a2e2bd854d0e74
nodilod <93993213+nodilod@users.noreply.github.com>
2022-03-16 12:13:00 +0100

    feat - detail navire
    ai

--- front/src/features/composent/navire/ListNavires.js
+++ front/src/features/composent/navire/ListNavires.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 const ListNavires = () => {
-    return (
-<<<<<<< Updated upstream
-        <div>
-            <h1>UNE LISTE DE NAVIRES</h1>
-=======
-        <div className="listBato">
 
+    return (
+        <div className="listBato">
             <Card className="card" sx={{maxWidth: 345}}>
                 <CardMedia
                     component="img"
@@ -21,16 +17,14 @@ const ListNavires = () => {
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         ici description du bato. This stigma has only been raised by a virtual queen.
-
                     </Typography>
                 </CardContent>
-                <CardActions >
+                <CardActions>
                     <Button className="lienCard" size="small">Voir le detail du navire</Button>
                 </CardActions>
             </Card>
->>>>>>> Stashed changes
         </div>
-    );
+    )
 };
 
 export default ListNavires;
