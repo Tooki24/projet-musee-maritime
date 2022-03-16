0828e7ec66d1417133826d74b8c638e7166985b0
nodilod <93993213+nodilod@users.noreply.github.com>
2022-03-16 11:28:36 +0100

    affichage liste bato

--- front/src/features/composent/navire/ListNavires.js
+++ front/src/features/composent/navire/ListNavires.js
@@ -1,11 +1,83 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "../../../assets/style/ListNavires.css"
 
 const ListNavires = () => {
+
+
     return (
-        <div>
-            <h1>UNE LISTE DE NAVIRES</h1>
+        <div className="listBato">
+
+            <Card className="card" sx={{maxWidth: 345}}>
+                <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="140"
+                    image="./Bato/bato.jpg"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Nom Bato
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        ici description du bato. This stigma has only been raised by a virtual queen.
+
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Voir le detail du navire</Button>
+                </CardActions>
+            </Card>
+
+            <Card className="card" sx={{maxWidth: 345}}>
+                <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="140"
+                    image="./Bato/bato.jpg"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Nom Bato
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        ici description du bato. This stigma has only been raised by a virtual queen.
+
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Voir le detail du navire</Button>
+                </CardActions>
+            </Card>
+
+            <Card className="card" sx={{maxWidth: 345}}>
+                <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="140"
+                    image="./Bato/bato.jpg"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Nom Bato
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        ici description du bato. This stigma has only been raised by a virtual queen.
+
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Voir le detail du navire</Button>
+                </CardActions>
+            </Card>
         </div>
     );
+
 };
 
 export default ListNavires;
