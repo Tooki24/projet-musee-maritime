d83c08c07316f2807b6c4235806f47969fff209d
Sami LAMTIOUI <sami.lamtioui@etudiant.univ-lr.fr>
2022-03-21 11:52:53 +0100

    feat Map

--- front/src/features/page/HomePage.js
+++ front/src/features/page/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BannerHome from "../composent/Banner/BannerHome";
+import Map from "../composent/Map";
 
 const HomePage = () => {
     return (
         <div>
             <BannerHome/>
+            <Map />
             <h1>page</h1>
         </div>
     );
