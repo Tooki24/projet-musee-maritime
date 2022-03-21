c4cfe22ad95cbccec90576e895933b29cbf3ddbc
Mathis Fumel <mathis.fumel@etudiant.univ-lr.fr>
2022-03-21 11:55:47 +0100

    MAJ Router

--- front/src/features/page/NaviresPage.js
+++ front/src/features/page/NaviresPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import BannerOtherPage from "../composent/Banner/BannerOtherPage";
 import ListNavires from "../composent/navire/ListNavires";
+import {Outlet} from "react-router-dom";
 
-const NaviresListPage = () => {
+const NaviresPage = () => {
     return (
         <div>
             <BannerOtherPage title={"Liste de navires"}/>
-            <ListNavires/>
-
+            <Outlet/>
         </div>
     );
 };
 
-export default NaviresListPage;
+export default NaviresPage;
